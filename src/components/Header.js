a5cb41ed51927f456e925393f7306c53241c0873
locoxelmillo <renzoaqc@gmail.com>
2023-10-23 21:53:35 +1300

    first

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 // Import Assets
 import profile from '../assets/profile.png';
+import video from '../assets/Presentacion.mp4';
 
 const Header = () => {
     
     return (
         <section className='header'>
-{/*         <video autoPlay loop muted className='video-background'>
-                <source src={videoBg} type='video/mp4' />
-            </video>  */}
-            <img src={profile} alt="Renzoaqc" />
+        <video autoPlay loop muted className='video-background'>
+                <source src={video} type='video/mp4' />
+            </video> 
             
+            <img src={profile} alt="Renzoaqc" />
+            <div class="overlay"></div>
             <div className='header__content'>
                 <h1>Hi, I'm Renzo Quispe</h1>
                 <p>Industrial Automation Engineer</p>
